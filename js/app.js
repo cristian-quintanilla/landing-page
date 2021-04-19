3315583298849b2afdfa1e333b99c9161ff3fcf1
Cristian Quintanilla <cristiancbtis130@gmail.com>
2021-04-19 08:28:47 -0500

    LandingPage

---
+++ js/app.js
@@ -0,0 +1,8 @@
+(function(){
+	var navButton = document.querySelector("#menu");
+		var navUl = document.querySelector(".nav__ul");
+		function toggleMobileMenu() {
+			navUl.classList.toggle("nav__hide-ul");
+		}
+		navButton.onclick = toggleMobileMenu;
+})();

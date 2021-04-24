a3d934ecb940cf2cf27ac5764e325254e4f6737d
Cristian Quintanilla <cristiancbtis130@gmail.com>
2021-04-23 23:05:49 -0500

    Font Size fixed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 (function(){
 	var navButton = document.querySelector("#menu");
-		var navUl = document.querySelector(".nav__ul");
-		function toggleMobileMenu() {
-			navUl.classList.toggle("nav__hide-ul");
-		}
-		navButton.onclick = toggleMobileMenu;
+	var navUl = document.querySelector(".nav__ul");
+	function toggleMobileMenu() {
+		navUl.classList.toggle("nav__hide-ul");
+	}
+	navButton.onclick = toggleMobileMenu;
 })();
